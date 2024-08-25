c60e6e51ddbc6b6750a17355f54d7b41abe663a4
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-08-25 20:29:59 +0300

    feat: add random color functionality
    each hover on the square div produce a different color

--- app.js
+++ app.js
@@ -11,10 +11,29 @@ let createGrid = async () => {
             rowCont.append(squareDiv);
         }
     }
-    
+}
+
+let randomizeColor = () => {
+    //randomize from 0 to 1677215 (complete hex range 000000 to FFFFFF)
+    let red = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let sum = red+green+blue;
+    let color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    return [color,sum];
 }
 
 createGrid();
+let num = 0;
 mainCont.addEventListener("mouseover",(e)=>{
-    e.target.classList.add("bg-color-change");
+    // e.target.classList.add("bg-color-change");
+    let [color,colorSum] = randomizeColor();
+    
+    console.log(color,colorSum);
+    
+    if(colorSum < 200){
+        console.log(color + "gowa");
+        color = randomizeColor();
+    }
+    e.target.style.backgroundColor = color;
 })
