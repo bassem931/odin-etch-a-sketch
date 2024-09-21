b55c0ddf336c3a552335007f338ea402aa7a34d0
Bassem Taher <87501973+bassem931@users.noreply.github.com.>
2024-09-21 11:41:28 +0300

    feat: add submit button to reset grid
    reset button with option to resize the grid

--- app.js
+++ app.js
@@ -1,39 +1,62 @@
-let mainCont = document.querySelector(".main-cont");
+let etchCont = document.querySelector(".etch-cont");
 
-let createGrid = async () => {
-    for (let row = 0; row < 16 ;row++) {
+
+let createGrid = (side_length) => {
+    let gridCont = document.createElement("div");
+    gridCont.classList.add("grid-cont")
+    for (let row = 0; row < side_length ;row++) {
         let rowCont = document.createElement("div");
         rowCont.classList.add("row-cont");
-        mainCont.append(rowCont);
-        for (let i = 0; i < 16;i++) {
+        gridCont.append(rowCont);
+        for (let i = 0; i < side_length;i++) {
             let squareDiv = document.createElement("div");
             squareDiv.classList.add("square-div");
             rowCont.append(squareDiv);
         }
     }
+    etchCont.append(gridCont);
 }
 
 let randomizeColor = () => {
-    //randomize from 0 to 1677215 (complete hex range 000000 to FFFFFF)
-    let red = Math.floor(Math.random()*255);
-    let blue = Math.floor(Math.random()*255);
-    let green = Math.floor(Math.random()*255);
-    let sum = red+green+blue;
-    let color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
-    return [color,sum];
+    etchCont.addEventListener("mouseover",(e)=>{
+        //randomize from 0 to 1677215 (complete hex range 000000 to FFFFFF)
+        let red = Math.floor(Math.random()*255);
+        let blue = Math.floor(Math.random()*255);
+        let green = Math.floor(Math.random()*255);
+        let color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+        e.target.style.backgroundColor = color;
+    })
 }
 
-createGrid();
-let num = 0;
-mainCont.addEventListener("mouseover",(e)=>{
-    // e.target.classList.add("bg-color-change");
-    let [color,colorSum] = randomizeColor();
+let getSideLength = () =>{
+    let root = document.querySelector(':root');
+    let popupForm = document.querySelector(".popup-form");
     
-    console.log(color,colorSum);
+    popupForm.addEventListener("submit",(ev)=>{
+        ev.preventDefault();
+        let length = window.prompt("enter the number of squares per side for the new grid");
+
+        while(Number.isNaN(parseInt(length))){
+            console.log(Number.isNaN(parseInt(length)));
+            length = window.prompt("enter the number of squares per side for the new grid");
+        }
+        
+        root.style.setProperty('--square-side',`${40/length}vw`);
+        document.querySelector(".grid-cont").remove();
+        createGrid(length);
+    })
     
-    if(colorSum < 200){
-        console.log(color + "gowa");
-        color = randomizeColor();
-    }
-    e.target.style.backgroundColor = color;
-})
+}
+
+function initializeEtch() {
+    createGrid(16);
+    randomizeColor();
+    getSideLength();
+}
+
+initializeEtch();
+
+
+// let length = window.prompt("enter the number of squares per side for the new grid");
+// console.log(typeof(parseInt(length)));
+
